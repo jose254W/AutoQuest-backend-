95fdf371a76d7e7ba01a25ee4c6a0dd3db228fa7
jose254W <josephwaweru069@gmail.com>
2023-05-06 00:34:58 +0300

    debug graphql

--- src/migrations/1683195451992-migrations.ts
+++ src/migrations/1683195451992-migrations.ts
@@ -2,10 +2,21 @@ import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class Migrations1683195451992 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
-    queryRunner.createDatabase('railway', true);
+    await queryRunner.query(`
+      CREATE TABLE cars (
+        id uuid PRIMARY KEY,
+        name varchar(255),
+        daily_price numeric,
+        monthly_price numeric,
+        mileage varchar(255),
+        gas varchar(255),
+        gear_type varchar(255),
+        thumbnail_url varchar(255)
+      )
+    `);
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    queryRunner.dropDatabase('railway', true);
+    await queryRunner.query(`DROP TABLE cars`);
   }
 }
